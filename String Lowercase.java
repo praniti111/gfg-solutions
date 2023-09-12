e279cd81796bbb72321b42cef0144a8e4de8789b
Praniti Parmar <pranitip111@gmail.com>
2023-09-12 23:43:10 +0530

    Create String Lowercase.java

---
+++ String Lowercase.java
@@ -0,0 +1,9 @@
+/* Q>: https://practice.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1?page=1&status[]=solved&sortBy=submissions */
+
+
+class Solution {
+    static String toLower(String S) {
+        // code here
+        return S.toLowerCase();
+    }
+}
